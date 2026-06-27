542b53ee2cbe36a879a80da16511175259adb0a7
Vennela-ai <vennelamarri.ai@gmail.com>
2026-06-27 11:27:42 +0530

    Implemented secure authentication and dashboard

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from models import User, db
 
 app = Flask(__name__)
@@ -15,8 +16,21 @@ with app.app_context():
 def home():
     return render_template("home.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password):
+            return redirect(url_for("dashboard"))
+
+        return "Invalid Email or Password!"
+
     return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -26,7 +40,10 @@ def register():
 
         name = request.form["name"]
         email = request.form["email"]
-        password = request.form["password"]
+        password = generate_password_hash(request.form["password"])
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return "Email already registered!"
 
         new_user = User(
             name=name,
@@ -40,6 +57,9 @@ def register():
         return "Registration Successful!"
 
     return render_template("register.html")
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
